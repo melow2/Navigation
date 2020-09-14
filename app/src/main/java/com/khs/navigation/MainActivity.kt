f6542a17c818da482f2489ae7591db43a1b9c7f6
happiness <melow2@naver.com>
2020-09-14 10:47:06 +0900

    [DEV] Project Setup

--- app/src/main/java/com/khs/navigation/MainActivity.kt
+++ app/src/main/java/com/khs/navigation/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.khs.navigation
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.khs.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
